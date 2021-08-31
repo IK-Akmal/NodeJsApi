eb915f62bb5c8f0d21a8c023c60d6b3153c5534a
E.N.D <isomadinovakmal1997@gmail.com>
2021-08-31 11:49:10 +0500

    add post api

--- main.js
+++ main.js
@@ -3,35 +3,60 @@
 const port = 9999;
 const statusNotFound = 404;
 const statusOk = 200;
-const posts = []; 
+const statusBadRequest = 400;
+
+let nextId = 1;
+const posts = [];
 
 const http = require('http');
 
 const methods = new Map();
 
 //Get post api
-methods.set('/posts.get', (request, response)=>{
-    response.writeHead(statusOk,{'Content-Type': 'application/json'});
+methods.set('/posts.get', (request, response) => {
+    response.writeHead(statusOk, { 'Content-Type': 'application/json' });
     response.end(JSON.stringify(posts));
 });
 
-methods.set('/posts.getById', (request, response)=>{});
-methods.set('/posts.post', (request, response)=>{});
-methods.set('/posts.edit', (request, response)=>{});
-methods.set('/posts.delete', (request, response)=>{});
+methods.set('/posts.getById', (request, response) => { });
+
+methods.set('/posts.post', (request, response) => {
+    const { searchParams } = new URL(request.url, `http://${request.headers.host}`);
+
+    if (!searchParams.has('content')) {
+        response.writeHead(statusBadRequest);
+        response.end();
+        return;
+    }
+    const content = searchParams.get('content');
+
+    const post = {
+        id: nextId++,
+        content: content,
+        created: Date.now(),
+    };
+
+    posts.unshift(post);
+    response.writeHead(statusOk, { 'Content-Type': 'application/json' });
+    response.end(JSON.stringify(post));
+});
+
+methods.set('/posts.edit', (request, response) => { });
+
+methods.set('/posts.delete', (request, response) => { });
 
 const server = http.createServer((request, response) => {
 
-    const {pathname, searchParams} = new URL(request.url,`http://${request.headers.host}`);
+    const { pathname } = new URL(request.url, `http://${request.headers.host}`);
 
     const method = methods.get(pathname);
-    if(method === undefined){
+    if (method === undefined) {
         response.writeHead(statusNotFound);
         response.end();
         return;
     }
 
-    method(request,response);
+    method(request, response);
 
 });
 
